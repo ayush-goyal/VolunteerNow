8ec51187a462f81fdd7b3554d26807c17d47d12d
ayush-goyal <agoyal2001@gmail.com>
2018-01-06 13:03:25 -0500

    Initial Commit

---
+++ VolunteerNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VolunteerNow
+//
+//  Created by Ayush Goyal on 1/6/18.
+//  Copyright © 2018 Summit Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
