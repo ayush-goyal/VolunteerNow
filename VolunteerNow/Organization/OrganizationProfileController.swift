c779d508a11c6581f7f988382e2c67446d969bc8
ayush-goyal <agoyal2001@gmail.com>
2018-01-23 23:35:31 -0500

    Add new organization icons

---
+++ VolunteerNow/Organization/OrganizationProfileController.swift
@@ -0,0 +1,9 @@
+//
+//  OrganizationProfileController.swift
+//  VolunteerNow
+//
+//  Created by Ayush Goyal on 1/23/18.
+//  Copyright © 2018 Summit Labs. All rights reserved.
+//
+
+import Foundation
